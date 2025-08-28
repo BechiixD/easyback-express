d74c95aa20c9a5cdad5d3a8e6bdd479ae1ca5b8a
BechiixD <BechiixD@users.noreply.github.com>
2025-08-28 11:52:48 -0300

    Example of endpoint working

--- generator.js
+++ generator.js
@@ -1,1 +1,25 @@
 // This file will take the JSON configuration and the server template and create a new Express server file with the foundations of the backend
+
+import express from "express";
+import { randomUUID } from "crypto";
+// import { template } from "/templates/server.js";
+import example from "./projects/example.json" with { type: "json" };
+
+const app = express();
+
+app.disable("x-powered-by");
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const BACKEND_URL = process.env.URL || `https://localhost:${PORT}`;
+
+example.endpoints.map((endpoint) => {
+  const { method, path, response } = endpoint;
+  app[method.toLowerCase()](`${path}`, (req, res) => {
+    console.log("at least it works");
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server runing on ${BACKEND_URL}`);
+});
